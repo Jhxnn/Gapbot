040e54f7622b0bbe04ca9057d0324efde6e2a234
Hilixx <jhonalizan@gmail.com>
2025-06-22 10:49:51 -0300

    first

---
+++ src/main/java/com/Gapbot/GapbotApplication.java
@@ -0,0 +1,13 @@
+package com.Gapbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GapbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GapbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gapbot/GapbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gapbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GapbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
